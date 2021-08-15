7bb0d582b224b52e589b3ec204590cb22c8c1a42
Nuno Fachada <nfachada@laseeb.org>
2021-08-15 20:09:17 +0100

    Start rewrite of clugen(), currently in clugenTNG()
    At the moment it only determines cluster sizes

--- src/CluGen.jl
+++ src/CluGen.jl
@@ -66,6 +66,138 @@ function generatePoint(numDims::Integer,
 end
 
 """
+Determine cluster sizes.
+"""
+function cluster_sizes(num_clusters::Integer, total_points::Integer, allow_empty::Bool)
+
+    # Determine number of points in each cluster using the half-normal
+    # distribution (with std=1)
+    clust_num_points = abs.(randn(num_clusters))
+    clust_num_points = clust_num_points / sum(clust_num_points)
+
+    # For consistency with other clugen implementations, rounding ties move away from zero
+    clust_num_points = round.(Int, total_points * clust_num_points, RoundNearestTiesAway)
+
+    # Make sure total points is respected
+    while sum(clust_num_points) < total_points
+        imin = argmin(clust_num_points)
+        clust_num_points[imin] += 1
+    end
+    while sum(clust_num_points) > total_points
+        imax = argmax(clust_num_points)
+        clust_num_points[imax] -= 1
+    end
+
+    # If empty clusters are not allowed, make sure there aren't any
+    if !allow_empty
+
+        # Find empty clusters
+        empty_clusts = findall(x -> x == 0, clust_num_points)
+
+        # If there are empty clusters...
+        if length(empty_clusts) > 0
+
+            # Go through the empty clusters...
+            for i0 in empty_clusts
+
+                # ...get a point from the largest cluster and assign it to the
+                # current empty cluster
+                imax = argmax(clust_num_points)
+                clust_num_points[imax] -= 1
+                clust_num_points[i0] += 1
+
+            end
+        end
+    end
+
+    return clust_num_points
+
+end
+
+"""
+    clugen()
+
+Create clusters.
+"""
+function clugenTNG(
+    num_dims::Integer,
+    num_clusters::Integer,
+    total_points::Integer,
+    direction::AbstractArray{<:Number, 1},
+    angle_std::Number,
+    cluster_sep::AbstractArray{<:Number, 1},
+    line_length::Number,
+    line_length_std::Number,
+    lateral_std::Number;
+    cluster_offset::Union{AbstractArray{<:Number, 1}, Nothing} = nothing,
+    point_dist::String = "unif",
+    point_offset::String = "nd",
+    allow_empty::Bool = false)
+
+    # ############### #
+    # Validate inputs #
+    # ############### #
+
+    if num_dims < 2
+        # TODO: Why not support 1D?
+        throw(ArgumentError("CluGen only supports two or more dimensions (`num_dims` < 2)"))
+    end
+
+    if norm(direction) == float(0)
+        throw(ArgumentError("`direction` must have magnitude > 0"))
+    end
+
+    if cluster_offset === nothing
+        cluster_offset = zeros(Float64, num_dims)
+    elseif length(cluster_offset) != num_dims
+        throw(ArgumentError(
+            "Length of `cluster_offset` must be equal to `num_dims` " *
+            "($(length(cluster_offset)) != $num_dims)"))
+    end
+
+    clusep_len = length(cluster_sep)
+    if clusep_len != num_dims
+        throw(ArgumentError(
+            "Length of `cluster_sep` must be equal to `num_dims` " *
+            "($clusep_len != $num_dims)"))
+    end
+
+    dir_len = length(direction)
+    if dir_len != num_dims
+        throw(ArgumentError(
+            "Length of `direction` must be equal to `num_dims` " *
+            "($dir_len != $num_dims)"))
+    end
+
+    if (point_dist != "unif") && (point_dist != "norm")
+        throw(ArgumentError("`point_dist` has to be either \"unif\" or \"norm\""))
+    end
+
+    if (point_offset != "nd") && (point_offset != "(n-1)d")
+        throw(ArgumentError("point_offset has to be either \"nd\" or \"(n-1)d)\""))
+    end
+
+    if !allow_empty && total_points < num_clusters
+        throw(ArgumentError(
+            "A total of $total_points points is not enough for " *
+            "$num_clusters non-empty clusters"))
+    end
+
+    # ##################### #
+    # Algorithm starts here #
+    # ##################### #
+
+    # Normalize base direction
+    dir_unit = normalize(direction)
+
+    # Determine cluster sizes
+    clust_num_points = cluster_sizes(num_clusters, total_points, allow_empty);
+
+    clust_num_points, sum(clust_num_points)
+
+end
+
+"""
     clugen()
 
 Create clusters.
@@ -140,7 +272,7 @@ function clugen(numDims::Integer,
         to_add = argmin(retPointCountPerCluster)[1]
         retPointCountPerCluster[to_add] += 1
     end
-    # Make sure the number of points is not moe than the totalPoints
+    # Make sure the number of points is not more than the totalPoints
     while (sum(retPointCountPerCluster) > totalPoints)
         to_add = argmax(retPointCountPerCluster)[1]
         retPointCountPerCluster[to_add] -= 1
