6f61330433e67c8f51a4c3df82c11b64d3fd3faa
Nuno Fachada <faken@fakenmc.com>
2021-08-23 18:18:59 +0100

    Add tests for clupoints_d()

--- test/clupoints_d.jl
+++ test/clupoints_d.jl
@@ -2,4 +2,33 @@
 # Distributed under the MIT License (See accompanying file LICENSE or copy
 # at http://opensource.org/licenses/MIT)
 
-# TODO
+# Number of line directions to test
+ndirs = 3
+
+# Number of line centers to test
+ncts = 3
+
+# Test clupoints_d
+@testset "clupoints_d" begin
+    @testset "nd=$nd, tpts=$tpts, seed=$(Int(rng.seed[1])), lat_std=$lat_std, length=$length, dir=$dir, ctr=$ctr" for
+        # Only for num_dims > 1
+        nd in filter((x) ->  x > 1, num_dims),
+        tpts in total_points,
+        rng in rngs,
+        lat_std in lat_stds,
+        length in line_lengths,
+        dir in get_vecs(rng, ndirs, nd),
+        ctr in get_vecs(rng, ncts, nd)
+
+        # Create some point projections
+        proj_dist2ctr = length .* rand(rng, tpts) .- length / 2;
+        proj = ctr' .+ proj_dist2ctr * dir'
+
+        # Check that the clupoints_d function runs without warnings
+        pts = @test_nowarn CluGen.clupoints_d(proj, lat_std, dir, ctr, rng)
+
+        # Check that number of points is the same as the number of projections
+        @test size(pts) == size(proj)
+
+    end
+end
