e6aa8238e49e7472f353d8008991c1a6b67ba475
Nuno Fachada <faken@fakenmc.com>
2023-06-15 21:22:55 +0100

    clumerge(): cleanup and add comments

--- src/helper.jl
+++ src/helper.jl
@@ -186,7 +186,6 @@ function fix_num_points!(
     return clu_num_points
 end
 
-
 mutable struct FieldInfo
     type::Type
     ncol::Integer
@@ -198,49 +197,44 @@ end
 In progress.
 
 * out can be "namedtuple" or "dict"
-* join can be "intersect" or "union"
 """
 function clumerge(
     data::Union{NamedTuple,Dict}...;
-    out::Symbol=:NamedTuple,
+    output_type::Symbol=:NamedTuple,
     fields::AbstractSet{Symbol}=Set((:points, :clusters)),
-    clusters_field::Union{Symbol,Nothing} = :clusters
+    clusters_field::Union{Symbol,Nothing}=:clusters,
 )::Union{NamedTuple,Dict}
-
+    # Number of elements in each array the merged dataset
     numel::Integer = 0
+
+    # Contains information about each field
     fields_info::Dict{Symbol,FieldInfo} = Dict()
-    output::Dict{Symbol, Any} = Dict()
+
+    # Merged dataset to ouput, initially empty
+    output::Dict{Symbol,Any} = Dict()
 
     # If a clusters field is given, it must exist in the fields to merge
     if clusters_field !== nothing && !(clusters_field in fields)
-        throw(
-            ArgumentError(
-                "`fields` parameter does not contain `$clusters_field`"
-            ),
-        )
+        throw(ArgumentError("`fields` parameter does not contain `$clusters_field`"))
+    end
+
+    # Check that the output type is either :NamedTuple or :Dict
+    if output_type != :NamedTuple && output_type != :Dict
+        throw(ArgumentError("`output_type` must be :NamedTuple or :Dict"))
     end
 
     # Cycle through data items
     for dt in data
 
         # Number of elements in the current item
-        numel_i::Union{Integer, Nothing} = nothing
+        numel_i::Union{Integer,Nothing} = nothing
 
         # Cycle through fields for the current item
         for field in fields
-
             if !haskey(dt, field)
-                throw(
-                    ArgumentError(
-                        "Data item does not contain required field `$field`"
-                    ),
-                )
+                throw(ArgumentError("Data item does not contain required field `$field`"))
             elseif field == clusters_field && !(eltype(dt[clusters_field]) <: Integer)
-                throw(
-                    ArgumentError(
-                        "`$clusters_field` must contain integer types"
-                    ),
-                )
+                throw(ArgumentError("`$clusters_field` must contain integer types"))
             end
 
             # Get the field value
@@ -262,7 +256,7 @@ function clumerge(
                 # elements
                 throw(
                     ArgumentError(
-                        "Data item contains fields with different sizes ($numel_tmp != $numel_i)"
+                        "Data item contains fields with different sizes ($numel_tmp != $numel_i)",
                     ),
                 )
             end
@@ -280,15 +274,13 @@ function clumerge(
                 # previous data items
                 if size(value, 2) != fields_info[field].ncol
                     # Number of columns must be the same
-                    throw(
-                        ArgumentError(
-                            "Dimension mismatch in field `$field`"
-                        ),
-                    )
+                    throw(ArgumentError("Dimension mismatch in field `$field`"))
                 end
 
                 # Get the common supertype
-                fields_info[field].type = promote_type(eltype(value), fields_info[field].type)
+                fields_info[field].type = promote_type(
+                    eltype(value), fields_info[field].type
+                )
             end
         end
 
@@ -298,87 +290,53 @@ function clumerge(
 
     # Initialize output dictionary fields with room for all items
     for ifield in fields_info
-        output[ifield.first] = Array{ifield.second.type}(undef, numel, ifield.second.ncol)
+        output[ifield.first] = if ifield.second.ncol == 1
+            Array{ifield.second.type}(undef, numel)
+        else
+            Array{ifield.second.type}(undef, numel, ifield.second.ncol)
+        end
     end
 
     # Copy items from input data to output dictionary, field-wise
     copied::Integer = 0
     last_cluster::Integer = 0
 
+    # Create merged output
     for dt in data
 
+        # How many elements to copy for the current data item?
         tocopy::Integer = size(getindex(dt, first(fields)), 1)
 
+        # Cycle through each field and its information
         for ifield in fields_info
+
+            # Copy elements
             output[ifield.first][(copied + 1):(copied + tocopy), :] =
-            if ifield.first == clusters_field
+                if ifield.first == clusters_field
 
-                old_clusters = unique(getindex(dt, clusters_field))
-                new_clusters = (last_cluster + 1):(last_cluster + length(old_clusters))
-                mapping = Dict(zip(old_clusters, new_clusters))
-                last_cluster = new_clusters[end]
-                [mapping[val] for val in getindex(dt, clusters_field)]
+                    # If this is a clusters field, update the cluster IDs
+                    old_clusters = unique(getindex(dt, clusters_field))
+                    new_clusters = (last_cluster + 1):(last_cluster + length(old_clusters))
+                    mapping = Dict(zip(old_clusters, new_clusters))
+                    last_cluster = new_clusters[end]
+                    [mapping[val] for val in getindex(dt, clusters_field)]
 
-            else
-                getindex(dt, ifield.first)
-            end
-
+                else
+                    # Otherwise just copy the elements
+                    getindex(dt, ifield.first)
+                end
         end
 
+        # Update how many were copied so far
         copied += tocopy
-
     end
 
-    return if out == :NamedTuple
+    # Return result, either as a named tuple or a dictionary
+    return if output_type == :NamedTuple
+        # Convert dictionary to named tuple
         (; output...)
     else
+        # Otherwise just return the dictionary
         output
     end
-
 end
-
-# Test 1:
-# o=clumerge((points=[3 3 3; 1 3 3; 0 0 -1.5], clusters=[1 1 2], stufff="fe"), Dict(:points => [1 -1 3; 10 1 2], :clusters=>[1 2]))
-
-# Test 2:
-# o0 = (points = 35 * rand(100,2) .- 20, clusters=ones(Int32, 100,1))
-# o1 = clugen(2, 5, 1000, [0 1; 0.25 0.75; 0.5 0.5; 0.75 0.25; 1 0], 0, [0, 0], 5, 0, 0.2; proj_dist_fn = "unif", point_dist_fn = "n", clusizes_fn = 500 * ones(Int32,5), clucenters_fn = [0 0; 2 -0.3; 4 -0.8; 6 -1.6; 8 -2.5])
-# o2 = clugen(2, 3, 500, [1,1], 0.1, [10,10], 23, 1, 0.2)
-# o3 = clumerge(o1, o2)
-# plot(o3.points[:, 1], o3.points[:, 2], seriestype = :scatter, group=o3.clusters, aspect_ratio = :equal )
-
-
-
-
-        # if !(haskey(dt, :points) && haskey(dt, :clusters))
-        #     throw(
-        #         ArgumentError(
-        #             "Input named tuples / dictionaries must have at least :points and :clusters keys"
-        #         ),
-        #     )
-        # end
-
-        # tpts_i, nd_i = size(getindex(dt, :points))
-
-        # if firstrun
-        #     nd = nd_i
-        # elseif nd != nd_i
-        #     throw(
-        #         ArgumentError(
-        #             "Dimension mismatch"
-        #         ),
-        #     )
-        # end
-
-        # tpts += tpts_i
-
-        # if length(getindex(dt, :clusters)) != tpts_i
-        #     throw(
-        #         ArgumentError(
-        #             "Number of points does not match length of :clusters column"
-        #         ),
-        #     )
-        # end
-
-        # if join != "minimal"
-        # end
